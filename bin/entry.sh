ecfa7f92bfe07cbec5613888fd759202b9a34d04
Angel Nunez Mencias <git@angelnu.com>
2018-12-31 11:57:41 +0100

    Fix

--- bin/entry.sh
+++ bin/entry.sh
@@ -53,7 +53,7 @@ ping -c1 $VXLAN_ROUTER_IP
 cp -av /etc/resolv.conf.dhclient* /etc_shared/resolv.conf
 
 FIRST_BOOT_MARKER=/etc_shared/booted
-if [ -e "$FIRST_BOOT_MARKER" ]; then
+if [ ! -e "$FIRST_BOOT_MARKER" ]; then
   touch $FIRST_BOOT_MARKER
   echo "First boot (init container): ending now."
   exit 0
