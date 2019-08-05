034c3223c052eae9a30b77e6c7618189f8c2f11d
Angel Nunez Mencias <git@angelnu.com>
2019-08-05 03:12:22 +0200

    move IP check earlier

--- bin/entry.sh
+++ bin/entry.sh
@@ -23,6 +23,12 @@ if [ ! -e "$DEFAULT_GW_REMOVED_MARKER" ]; then
   ip route del 0/0
 fi
 
+#After this point nothing should be reachable -> check
+if ping -c 1 -W 1000 8.8.8.8; then
+  echo "WE SHOULD NOT BE ABLE TO PING -> EXIT"
+  exit 255
+fi
+
 #derived settings
 OPENVPN_ROUTER_IP="$(dig +short $OPENVPN_ROUTER_NAME @${DNS_ORG})"
 GW_ORG=$(route |awk '$1=="default"{print $2}')
@@ -32,12 +38,6 @@ NAT_ENTRY="$(grep $(hostname) /config/nat.conf||true)"
 ip link add vxlan0 type vxlan id $VXLAN_ID dev eth0 dstport 0 || true
 bridge fdb append to 00:00:00:00:00:00 dst $OPENVPN_ROUTER_IP dev vxlan0
 ip link set up dev vxlan0
-
-#After this point nothing should be reachable -> check
-if ping -c 1 -W 1000 8.8.8.8; then
-  echo "WE SHOULD NOT BE ABLE TO PING -> EXIT"
-  exit 255
-fi
 
 #Configure IP and default GW though the VPN docker
 if [ -z "$NAT_ENTRY" ]; then
